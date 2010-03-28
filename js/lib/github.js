4e166edda8d8b9a15da2f95fdbd47b2f7df58138
Eris <eris.discord@gmail.com>
2010-03-28 04:10:46 -0400

    added gist support to github.js and application code

--- js/lib/github.js
+++ js/lib/github.js
@@ -1,6 +1,14 @@
 (function() {
-  var gh_insecure_api_base = 'http://github.com/api/v2/json/';
-  var gh_secure_api_base   = 'https://github.com/api/2/json/';
+  var gh_api_base = {
+    github: {
+      insecure: 'http://github.com/api/v2/json/@',
+      secure: 'https://github.com/api/v2/json/@'
+    },
+    gist: {
+      insecure: 'http://gist.github.com/@.json',
+      secure: 'https://gist.github.com/@.json'
+    }
+  };
   
   var gh_next_callback_number = 0;
   function gh_next_callback_id() {
@@ -18,12 +26,8 @@
   
   var gh_unauth_instance = null;
   GitHub.unauth = function() {
-    if (gh_unauth_instance)
-      return gh_unauth_instance;
-    else {
-      gh_unauth_instance = new this(null, null);
-      return gh_unauth_instance;
-    }
+    if (!gh_unauth_instance) gh_unauth_instance = new this(null, null);
+    return gh_unauth_instance;
   }
   
   GitHub.api_uri = function(path, callback_id) {
@@ -34,25 +38,39 @@
     return this.unauth().call(path, callback);
   };
   
+  GitHub.gist = function(id, callback) {
+    return this.unauth().gist(id, callback);
+  }
+  
   // INSTANCE METHODS
   
   GitHub.prototype.api_uri = function(path, callback_id) {
     var login = this.login, token = this.token;
     var authenticated = !!login && !!token
     
+    var api, match;
+    
+    if (match = path.match(/^(gist|github):(.+)$/)) {
+      api  = match[1];
+      path = match[2];
+    }
+    else {
+      api = 'github';
+    }
+    
     var uri_base;
     var uri_params = '?callback=' + escape('GitHub.callbacks["' + callback_id + '"]');
     
     if (authenticated) {
-      uri_base    = gh_secure_api_base;
+      uri_base    = gh_api_base[api].secure;
       uri_params += '&login=' + escape(this.login)
                  +  '&token=' + escape(this.token);
     }
     else {
-      uri_base = gh_insecure_api_base;
+      uri_base = gh_api_base[api].insecure;
     }
     
-    return uri_base + escape(path) + uri_params;
+    return uri_base.replace('@', escape(path)) + uri_params;
   };
   
   GitHub.prototype.call = function(path, user_callback) {
@@ -69,9 +87,9 @@
     
     script.setAttribute('id',   callback_id);
     script.setAttribute('type', 'text/javascript');
-    script.setAttribute('src',  GitHub.api_uri(path, callback_id));
+    script.setAttribute('src',  this.api_uri(path, callback_id));
     
     head.appendChild(script);
-  };
+  }
   
 })();
