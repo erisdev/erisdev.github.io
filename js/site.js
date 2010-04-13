fca696955c62f8a158f4a3cf942ed43ceacc08b6
Eris <eris.discord@gmail.com>
2010-04-13 19:35:49 -0400

    refactored javascript a bit so the site is more usable without

--- js/site.js
+++ js/site.js
@@ -1,30 +1,54 @@
+(function() {
+// begin closure
+
+var TEMPLATES = {};
+
 $(function() {
+	$('.template').each(function(i, el) {
+		var $el = $(el), name = $el.attr('data-name'), text = $el.text();
+		TEMPLATES[name] = _.template(text);
+	});
+	console.log(TEMPLATES.repositories)
+	
 	$('#sections').tabs({
 		cache: true,
-		spinner: $('#spinner-tmpl').text()
+		spinner: 'Loading...'
 	});
 	
 	$('p > a[href^=http://gist.github.com/]:only-child').livequery(function() {
 		$(this).each(function(i, link) {
-			var match = $(link).attr('href').match(/^http:\/\/gist\.github\.com\/(\d+)$/i);
+			var $link = $(link), match = $(link).attr('href').match(/^http:\/\/gist\.github\.com\/(\d+)$/i);
 			if (!match) return;
 			GitHub.call('gist:' + match[1], function(data) {
-				$(link).parent().replaceWith(data.div);
+				$link.parent().replaceWith(data.div);
 			});
 		});
 	});
 	
-	GitHub.call('user/show/' + CONFIG.github.user, function(data) {
-		$('#profile-tmpl').render(data.user).appendTo($('#overview .profile').empty());
-	});
+	$('a.profile[data-user]').livequery(function() {
+		var $link = $(this), user = $link.attr('data-user');
+		$link.wrap('<h3>').removeClass('profile');
+		GitHub.call('user/show/' + user, function(data) {
+			$link.parent().after(TEMPLATES.profile({ user:data.user }));
+		});
+	}); // a.profile
 	
-	GitHub.call('repos/show/' + CONFIG.github.user, function(data) {
-		$('#repository-tmpl').render(data.repositories).appendTo($('#overview .repositories').empty());
-	});
+	$('a.repositories[data-user]').livequery(function() {
+		var $link = $(this), user = $link.attr('data-user');
+		$link.wrap('<h3>').removeClass('repositories');
+		GitHub.call('repos/show/' + user, function(data) {
+			$link.parent().after(TEMPLATES.repositories({ repositories:data.repositories }));
+		});
+	}); // a.repositories
 	
-	$.jTwitter(CONFIG.twitter.user, CONFIG.twitter.limit, function(tweets) {
-		$('#tweet-tmpl').render(tweets).appendTo($('#overview .twitter').empty());
-	})
+	$('a.twitter[data-user]').livequery(function() {
+		var $link = $(this), user = $link.attr('data-user');
+		$link.wrap('<h3>').removeClass('twitter');
+		$.jTwitter(user, function(data) {
+			_(data).each(function(tweet) { tweet.html = autolink(tweet.text) })
+			$link.parent().after(TEMPLATES.twitter({ tweets:data }));
+		});
+	}); // a.twitter
 	
 });
 
@@ -34,3 +58,6 @@ function autolink(text) {
 		'<a href="$&">$&</a>'
 	);
 }
+
+// end closure
+})();
