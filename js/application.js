b5eb802bf0b745859fdfbd4e718692610cd135b9
Eris <eris.discord@gmail.com>
2011-08-27 15:51:03 -0400

    github projects menu

--- js/application.js
+++ js/application.js
@@ -1,8 +1,30 @@
 $(function() {
+	var $projectsMenu = $();
 	
 	$('a.menu').click(function(event) {
 		$(this).parent('li').toggleClass('open');
 		return false;
 	});
 	
+	populateGitHubMenu('#github-projects')
 });
+
+function populateGitHubMenu(selector)
+{
+	var $menu = $(selector);
+	var user = $menu.data('user');
+	
+	$.github(user).repositories(function(data) {
+		_.each(data.repositories, function(r) {
+			if ( r.fork ) return;
+			
+			$('<li>').append(
+				$('<a>')
+				.text(r.name)
+				.attr('href', r.url)
+			).append(
+				$('<span>').append($('<small>').text(r.description))
+			).appendTo($menu);
+		})
+	});
+}
