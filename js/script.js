3c8331b16cc1cba4b7246125573105013f8b9494
Eris <eris.discord@gmail.com>
2011-10-11 17:15:11 -0400

    highlight link to current page with a different color

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
-/* Author:
+(function() {
 
-*/
+  function naiveUriCompare(a, b) {
+    a = a.replace(/\/?(?:[#\?].*)?$/, '');
+    b = b.replace(/\/?(?:[#\?].*)?$/, '');
+    return a === b;
+  }
 
+  $(function() {
+    $('a[href]').each(function(i, el) {
+      if ( naiveUriCompare(location.href, el.href) )
+        $(el).addClass('here');
+    });
+  });
 
-
-
-
-
-
+})()
