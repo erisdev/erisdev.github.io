9fe1cc7332bb5fb5a819ada8d048d6425e7ebf6f
Eris <eris.discord@gmail.com>
2011-10-11 23:40:14 -0400

    display post dates with day rather than second resolution

--- js/script.js
+++ js/script.js
@@ -12,7 +12,22 @@
         $(el).addClass('here').click(function(event) { event.preventDefault() });
     });
     
-    $('time').easydate();
+    $('time.datetime-relative').each(function(i, el) {
+      var $time = $(el);
+      var options = { };
+      
+      if ( $time.hasClass('datetime-date-only') )
+        options.units = [
+          { name: "today", limit: 86400 },
+          { name: "yesterday", limit: 172800, past_only: true },
+          { name: "tomorrow", limit: 172800, future_only: true },
+          { name: "day", limit: 604800, in_seconds: 86400 },
+          { name: "week", limit: 2629743, in_seconds: 604800  },
+          { name: "month", limit: 31556926, in_seconds: 2629743 },
+          { name: "year", limit: Infinity, in_seconds: 31556926 } ]
+      
+      $time.easydate(options);
+    });
   });
 
 })()
